bbaa302ab7f5332537dcbb6918d73f5cc0cdbef9
LEESojung1998 <80635289+LEESojung1998@users.noreply.github.com>
2023-02-16 15:26:53 +0800

    Update run_sample.sh

--- lasTEq/data/run_sample.sh
+++ lasTEq/data/run_sample.sh
@@ -1,3 +1,1 @@
-lasTEq bulk assign sample_alignment_sort.bam annotation.gtf long_read_data.txt \
-                  --attribute gene_id --reassign_mode long_read --rescue_short 1e-50 \
-                  --prior_change all --long_read_type matched
+lasTEq bulk assign sample_alignment_sort.bam annotation.gtf long_read_data.txt --attribute gene_id --reassign_mode long_read --rescue_short 1e-50 --prior_change all --long_read_type matched
